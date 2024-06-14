7fa0056b7e414413782b46d3228db364b41c1490
Yaroslav <yaroslav@keyoneinc.uk>
2024-06-14 18:55:07 +0100

    added Product.cpp defenition

--- cw32_task3/Product.cpp
+++ cw32_task3/Product.cpp
@@ -1,1 +1,57 @@
 #include "Product.h"
+
+Shop::Product::Product()
+{
+    title = "undefined";
+    price = 0.0;
+    manufacturer = "undefined";
+    age = 0;
+}
+
+Shop::Product::Product(string title, float price, string manufacturer, int age)
+{
+    this->title = title;
+    this->price = price;
+    this->manufacturer = manufacturer;
+    this->age = age;
+}
+
+void Shop::Product::setTitle(string title)
+{
+    this->title = title;
+}
+
+void Shop::Product::setPrice(float price)
+{
+    this->price = price;
+}
+
+void Shop::Product::setManufacturer(string manufacturer)
+{
+    this->manufacturer = manufacturer;
+}
+
+void Shop::Product::setAge(int age)
+{ 
+    this->age = age;
+}
+
+string Shop::Product::getTitle() const
+{
+    return title;
+}
+
+float Shop::Product::getPrice() const
+{
+    return price;
+}
+
+string Shop::Product::getManufacturer() const
+{
+    return manufacturer;
+}
+
+int Shop::Product::getAge() const
+{
+    return age;
+}
