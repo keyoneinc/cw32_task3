ea4bc91122419334d105fd405b54a05ed74fb646
Yaroslav <yaroslav@keyoneinc.uk>
2024-06-14 20:04:49 +0100

    ready project with working lottery end of lesson

--- cw32_task3/main.cpp
+++ cw32_task3/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "Lotery.h"
 
 int main()
 {
+	Shop::Lotery Zabawa;
+	Zabawa.addItem(new Shop::Child::Toy("Ball", 200, "Nike", 12, "leather"));
+	Zabawa.addItem(new Shop::Child::Toy("Kite", 200, "Nike", 12, "cotton"));
 	
+	Zabawa.addItem(new Shop::Sport::Inventory("Snikers", 20000, "Nike", 10, "football", 0.5));
+
+	/*Zabawa.addItem(new Shop :: Entertainment :: Book("")*/
+
+	Zabawa.randItem();
 }
