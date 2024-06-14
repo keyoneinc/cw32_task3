fafd27db83615aa8d1527b7b72247cd85c5ccc35
Yaroslav <yaroslav@keyoneinc.uk>
2024-06-14 19:35:57 +0100

    added defetnition in Inventory.cpp

--- cw32_task3/Inventory.cpp
+++ cw32_task3/Inventory.cpp
@@ -1,1 +1,51 @@
 #include "Inventory.h"
+
+Shop::Sport::Inventory::Inventory()
+{
+	kind = "undefined";
+	weight = 0;
+}
+
+Shop::Sport::Inventory::Inventory(string title, float price, string manufacturer, int age, string kind, float weight)
+	: Product(title, price, manufacturer, age)
+{
+	this->kind = kind;
+	this->weight = weight;
+}
+
+void Shop::Sport::Inventory::setKind(string kind)
+{
+	this->kind = kind;
+}
+
+void Shop::Sport::Inventory::setWeight(float weight)
+{
+	this->weight = weight;
+}
+
+string Shop::Sport::Inventory::getKind() const
+{
+	return kind;
+}
+
+float Shop::Sport::Inventory::getWeight() const
+{
+	return weight;
+}
+
+void Shop::Sport::Inventory::show() const
+{
+	cout << "\t" << type() << endl;
+	cout << "Title: " << title << endl;
+	cout << "Kind: " << kind << endl;
+	cout << "Weight: " << weight << endl;
+	cout << "Price: " << price << endl;
+	cout << "Manufacturer: " << manufacturer << endl;
+	cout << "Age: " << age << endl << endl;
+
+}
+
+string Shop::Sport::Inventory::type() const
+{
+	return "Sport";
+}
